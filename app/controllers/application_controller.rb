f354a3af2e21774136c486d7da74150654f6b970
Wes Mehaffy <wesonweb@gmail.com>
2023-02-25 15:47:11 +0000

    adds user detail migration and view update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name nickname photo bio])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname photo bio])
+  end
 end
---
+++ db/migrate/20230225154201_add_details_to_user.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :photo, :string
+    add_column :users, :bio, :string
+  end
+end
