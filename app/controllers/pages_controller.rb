9605bce3390ffecb4e5dda5cf988ee071501481c
Wes Mehaffy <wesonweb@gmail.com>
2023-03-02 20:08:15 +0000

    adds basic search and returns the airport name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
 
   def home
+    if params[:query].present?
+      @airports = Airport.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @airports = Airport.all
+    end
   end
 end
