71e361e57c441e6c64b8357120e08b667211d0e3
Wes Mehaffy <wesonweb@gmail.com>
2023-03-03 16:36:46 +0000

    updates routes so that meetups are nested inside the right airport

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- resources :airports do
+  resources :airports do
   resources :facilities, only: [:index, :show]
   resources :tips, only: [:index]
+  resources :meetups
  end
 
- resources :meetups
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
