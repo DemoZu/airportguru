bc70719a224b62cf649a39331af5fa07c6c873a7
Wes Mehaffy <wesonweb@gmail.com>
2023-03-14 16:39:56 +0000

    creates simple_form for adding a tip and saves it

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -6,5 +6,20 @@ class TipsController < ApplicationController
     @tips = @airport.tips.order(likes: :desc) if present?
     @tip_categories = TipCategory.all
     @flights = current_user.flights if current_user.present?
+    @tip = Tip.new
+  end
+
+  def create
+    @airport = Airport.find(params[:airport_id])
+    @tip = Tip.new(tip_params)
+    return unless @tip.save
+
+    redirect_to airport_tips_path
+  end
+
+  private
+
+  def tip_params
+    params.require(:tip).permit(:title, :content, :user_id, :airport_id, :tip_category_id)
   end
 end
