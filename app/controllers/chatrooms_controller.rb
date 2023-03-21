a0f463a89c26072f3cfe1e99d9a8058a784f1f14
Wes Mehaffy <wesonweb@gmail.com>
2023-03-21 19:32:03 +0000

    updates css for flight info and airport info. Optimises images

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -28,18 +28,10 @@ class ChatroomsController < ApplicationController
   end
 
   def create
-    # @chatroom = Chatroom.new(chatroom_params)
     @chatroom = Chatroom.new
     @meetup = Meetup.find(params[:meetup_id])
-    @chatroom.name = current_user.nickname + " and #" + @meetup.user.nickname + " talking.."
-    # @chatroom.name = @meetup.content
-    # @meetup.content + "with #{meetup.user.nickname} and " + current_user.nickname
-    # if checkname_exist_name != @chatroom.name
+    @chatroom.name = current_user.nickname + " and #" + @meetup.user.nickname + " chatting..."
     if @chatroom.save
-      # @chatroom = Chatroom.find(params[:chatroom_id])
-      # @chatroom = Chatroom.find(1)
-      # add param Meetup.find(params[:meetup_id])
-      # @meetup = Meetup.find(params[:meetup_id])
       @message = Message.new
       @message.meetup = @meetup
       @message.chatroom = @chatroom
@@ -47,14 +39,12 @@ class ChatroomsController < ApplicationController
       @message.user = current_user
       if @message.save
         redirect_to "#{airport_chatrooms_path}/#{@chatroom.id}"
-        # /#{@chatroom.id}"
       else
         render "chatrooms/show", status: :unprocessable_entity
       end
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
-    # end
   end
 
   private
